8575c99b9786a15cbe1d9f535412b1a1c1a7df3f
Furt <lit_69@hotmail.com>
2012-01-11 00:43:24 -0600

    Updated who command

--- src/me/furt/CraftEssence/commands/WhoCommand.java
+++ src/me/furt/CraftEssence/commands/WhoCommand.java
@@ -24,22 +24,78 @@ public class WhoCommand implements CommandExecutor {
 					+ "You do not have permission to use /" + label);
 			return true;
 		}
-		
-		UserTable ut = plugin.getDatabase().find(UserTable.class).where()
-				.ieq("displyName", args[0]).findUnique();
-		if (ut != null) {
-			Player p = plugin.getServer().getPlayer(ut.getUserName());
-			sender.sendMessage(ChatColor.YELLOW + "-------- " + ChatColor.RED + "Stats" + ChatColor.YELLOW + " ---------");
-			sender.sendMessage(ChatColor.YELLOW + "Account Name: " + ut.getUserName());
-			sender.sendMessage(ChatColor.YELLOW + "Display Name: " + ut.getDisplyName());
-			sender.sendMessage(ChatColor.YELLOW + "Health: " + p.getHealth() + "/20");
-			sender.sendMessage(ChatColor.YELLOW + "IP: " + p.getAddress().getHostName());
-			sender.sendMessage(ChatColor.YELLOW + "Logins: " + ut.getLogins());
-			sender.sendMessage(ChatColor.YELLOW + "Is AFK: " + ut.isAfk());
-			sender.sendMessage(ChatColor.YELLOW + "Is Muted: " + ut.isMuted());
+
+		if (args.length == 1) {
+			UserTable ut = plugin.getDatabase().find(UserTable.class).where()
+					.ieq("displyName", args[0]).findUnique();
+			if (ut != null) {
+				Player p = plugin.getServer().getPlayer(ut.getUserName());
+				String mute;
+				if (ut.isMuted()) {
+					mute = "yes";
+				} else {
+					mute = "no";
+				}
+				String afk;
+				if (ut.isAfk()) {
+					afk = "yes";
+				} else {
+					afk = "no";
+				}
+				sender.sendMessage(ChatColor.YELLOW + "-------- "
+						+ ChatColor.RED + "Stats" + ChatColor.YELLOW
+						+ " ---------");
+				sender.sendMessage(ChatColor.YELLOW + "Account Name: "
+						+ ut.getUserName());
+				sender.sendMessage(ChatColor.YELLOW + "Display Name: "
+						+ ut.getDisplyName());
+				sender.sendMessage(ChatColor.YELLOW + "Health: "
+						+ p.getHealth() + "/20");
+				sender.sendMessage(ChatColor.YELLOW + "Hunger: "
+						+ p.getFoodLevel() + "/20");
+				sender.sendMessage(ChatColor.YELLOW + "IP: "
+						+ p.getAddress().getHostName());
+				sender.sendMessage(ChatColor.YELLOW + "Logins: "
+						+ ut.getLogins());
+				sender.sendMessage(ChatColor.YELLOW + "AFK: " + afk);
+				sender.sendMessage(ChatColor.YELLOW + "Muted: " + mute);
+			} else {
+				sender.sendMessage(CraftEssence.premessage
+						+ "Player not found.");
+			}
+			return true;
 		} else {
-			sender.sendMessage(CraftEssence.premessage + "Player not found.");
+			UserTable ut1 = plugin.getDatabase().find(UserTable.class).where()
+					.ieq("displyName", sender.getName()).findUnique();
+			Player p = plugin.getServer().getPlayer(ut1.getUserName());
+			String mute;
+			if (ut1.isMuted()) {
+				mute = "yes";
+			} else {
+				mute = "no";
+			}
+			String afk;
+			if (ut1.isAfk()) {
+				afk = "yes";
+			} else {
+				afk = "no";
+			}
+			sender.sendMessage(ChatColor.YELLOW + "-------- " + ChatColor.RED
+					+ "Stats" + ChatColor.YELLOW + " ---------");
+			sender.sendMessage(ChatColor.YELLOW + "Account Name: "
+					+ ut1.getUserName());
+			sender.sendMessage(ChatColor.YELLOW + "Display Name: "
+					+ ut1.getDisplyName());
+			sender.sendMessage(ChatColor.YELLOW + "Health: " + p.getHealth()
+					+ "/20");
+			sender.sendMessage(ChatColor.YELLOW + "Hunger: " + p.getFoodLevel()
+					+ "/20");
+			sender.sendMessage(ChatColor.YELLOW + "IP: "
+					+ p.getAddress().getHostName());
+			sender.sendMessage(ChatColor.YELLOW + "Logins: " + ut1.getLogins());
+			sender.sendMessage(ChatColor.YELLOW + "AFK: " + afk);
+			sender.sendMessage(ChatColor.YELLOW + "Muted: " + mute);
+			return true;
 		}
-		return true;
 	}
 }
